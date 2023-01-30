36897d72bd236b2a2abc96efe93eeb86654bad2f
unknown <reagan.rodriguez.a@gmail.com>
2023-01-30 13:53:30 +0000

    ist1

---
+++ starter/assets/js/questions.js
@@ -0,0 +1,9 @@
+// questions taken from https://www.javatpoint.com/javascript-mcq
+
+var questions=[
+[["1. Which type of JavaScript language is ___"],[["a. Object-Oriented"],["b. Object-Based"],["c. Assembly-language"],["d. High-level"]],["b. Object-Based"]],
+[["2. Which one of the following also known as Conditional Expression: ___"],[["a. Alternative to if-else"],["b. Switch statement"],["c. If-then-else statement"],["d. immediate if"]],["d. immediate if"]],
+[["3. In JavaScript, what is a block of statement? ___"],[["a. Conditional block"],["b. block that combines a number of statements into a single compound statement"],["c. both conditional block and a single statement"],["d. block that contains a single statement"]],["b. block that combines a number of statements into a single compound statement"]],
+[["4. When interpreter encounters an empty statements, what it will do: ___"],[["a. Shows a warning"],["b. Prompts to complete the statement"],["c. Throws an error"],["d. Ignores the statements"]],["d. Ignores the statements"]],
+[["5. Which of the following variables takes precedence over the others if the names are the same? ___"],[["a. Global variable"],["b. The local variable"],["c. The two of the above"],["d. None of the above"]],["b. The local variable"]]
+];
